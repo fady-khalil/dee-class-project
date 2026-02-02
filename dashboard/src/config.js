f5d200c539a20932481f77686ce6a29e8ef512df
ahmedmohame19 <96324257+ahmedmohame19@users.noreply.github.com>
2026-02-02 12:22:19 +0200

    edits

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -1,8 +1,8 @@
-// const BACKEND_URL = 'https://api.yalladad.com/api'
+const BACKEND_URL = 'https://apideclass.yamminelawfirm.com/api'
 
-const BACKEND_URL = 'http://localhost:5001/api'
+// const BACKEND_URL = 'http://localhost:5001/api'
 
 export default BACKEND_URL
 
-export const BASE_URL = 'http://localhost:5001'
-// export const BASE_URL = 'https://api.yalladad.com'
+// export const BASE_URL = 'http://localhost:5001'
+export const BASE_URL = 'https://apideclass.yamminelawfirm.com'
