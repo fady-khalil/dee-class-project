f7b0f336df3c64c6472f7d4652e4c9f0e68cabc8
fady-khalil <fady.khalil123@hotmail.com>
2026-02-03 16:35:15 +0200

    fix frontend url

--- d-class/src/Utilities/BASE_URL.js
+++ d-class/src/Utilities/BASE_URL.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://apideclass.yamminelawfirm.com/api";
 // const BASE_URL = "http://localhost:5001/api";
 
-export const FRONTEND_URL = "https://api.yamminelawfirm.com";
+export const FRONTEND_URL = "https://declass.yamminelawfirm.com";
 // export const FRONTEND_URL = "http://localhost:3001";
 // export const FRONTEND_URL = "https://apideclass.yamminelawfirm.com";
 
